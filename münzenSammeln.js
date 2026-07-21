197f93f1593e76be7b47f18f68c657c1cffe246a
Juju0904 <julter@gmx.de>
2026-07-21 18:10:02 +0200

    münzenSammeln schwarzer block als Spieler zum test

--- münzenSammeln.js
+++ münzenSammeln.js
@@ -1,6 +1,37 @@
-let münzen = 0;
-
-if () {
-    münzen++;
-    document.getElementById("münzenanzahl").textContent = münzen;
+let jonas = {
+    left: 100,
+};
+function setJonasPosition() {
+    $("jonas").css("left",jonas.left);
 }
+
+let punkte = 0;
+    function checkKollision() {
+        if (jonas.left === 300) {
+            punkte = punkte + 1;
+            $("münzenanzahl").text("Münzen: " + punkte);
+        }
+    }
+$(document).on("keydown", function (e) {
+    if (e.code === "KeyA") {
+        jonas.left = jonas.left - 10;
+        setJonasPosition();
+        checkKollision();
+    }
+    if (e.code === "KeyD") {
+        jonas.left = jonas.left + 10;
+        setJonasPosition();
+        checkKollision();
+    }
+    if (e.code === "KeyW") {
+        jonas.top = jonas.top - 10;
+        setJonasPosition();
+        checkKollision();
+    }
+    if (e.code === "KeyS") {
+        jonas.top = jonas.top + 10;
+        setJonasPosition();
+        checkKollision();
+    }
+});
+
